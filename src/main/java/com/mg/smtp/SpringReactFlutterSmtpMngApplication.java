938c215a4949d9764004dbd64c89e1526dfdbb90
VelkaressiaBlutkrone <deepestdark@gmail.com>
2026-02-23 17:43:30 +0900

    inital commit

---
+++ src/main/java/com/mg/smtp/SpringReactFlutterSmtpMngApplication.java
@@ -0,0 +1,13 @@
+package com.mg.smtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactFlutterSmtpMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactFlutterSmtpMngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mg/smtp/SpringReactFlutterSmtpMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mg.smtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactFlutterSmtpMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
